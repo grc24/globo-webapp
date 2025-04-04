ec8b615d13c70112ec52f2689a0e156c00911789
SEIDOU <david.seidou@iviplay.com>
2025-04-04 15:42:57 +0100

    Switch to using tfe_outputs fix error

--- variables.tf
+++ variables.tf
@@ -5,11 +5,6 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "test" {
-  type        = string
-  description = "test"
-}
-
 variable "prefix" {
   type        = string
   description = "(Required) Prefix to use for all resources in this module."
@@ -42,15 +37,6 @@ variable "api_key" {
   description = "(Required) API key for web app to talk to SaaS platform."
 }
 
-variable "public_subnets" {
-  type        = list(string)
-  description = "(Required) List of subnet IDs for EC2 instance deployments."
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "(Required) VPC ID of VPC for application deployment."
-}
 
 variable "tfe_organization" {
   type        = string
